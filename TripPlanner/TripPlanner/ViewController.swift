cd397f855d83b75ac81b218923016e6e1f7d6dec
user137759 <user137759@103-246-99-78.moboware.com>
2018-04-19 14:13:39 +1000

    initial project creation, syncing with GitHub

---
+++ TripPlanner/TripPlanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TripPlanner
+//
+//  Created by user137759 on 4/19/18.
+//  Copyright © 2018 user137759. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
